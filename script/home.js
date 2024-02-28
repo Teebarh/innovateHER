a438d9935be48a5275d9b70575daa000144b1627
ileri_oluwa_kiiye <aboladeilerioluwakiiye@gmail.com>
2024-02-29 00:52:42 +0100

    Added javascript. Made more nice changes

---
+++ script/home.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+  if (document.getElementById("nav").style.display == "block") {
+    document.getElementById("nav").style.display = "none";
+  } else {
+    document.getElementById("nav").style.display = "block";
+  }
+}
